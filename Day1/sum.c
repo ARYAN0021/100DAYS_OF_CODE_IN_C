cf8f82f7b05dfda73ba14451796f6216ae4d8e0d
Aryan Agarwal <77781900+ARYAN0021@users.noreply.github.com>
2025-08-30 23:31:11 +0530

    Create sum.c

---
+++ Day1/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main(){
+     int a,b,c;
+
+     printf("enter any number of your choice: ");
+     scanf("%d" , &a);
+
+      printf("enter any number of your choice: ");
+     scanf("%d" , &b);
+
+
+     c= a+b;
+     printf("sum of two number is: %d" , c);
+
+ }
