4bb3f767fbd192a3aa3c3dcc748b9e9f77610d10
Aryan Agarwal <77781900+ARYAN0021@users.noreply.github.com>
2025-08-31 16:40:55 +0530

    Create rectangle.c

---
+++ Day2/rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int l = 2;
+    int b = 3;
+
+    int area = l*b;
+    int perimeter = 2*(l+b);
+
+    printf("Area is :%d",area);
+    printf("perimeter is : %d",perimeter);
+
+}
