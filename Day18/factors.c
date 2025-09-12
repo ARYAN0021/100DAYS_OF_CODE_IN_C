1eba84fa1b21dd630c8ca34c17f600d8d4d967f6
Aryan Agarwal <77781900+ARYAN0021@users.noreply.github.com>
2025-09-13 02:46:46 +0530

    Create factors.c

---
+++ Day18/factors.c
@@ -0,0 +1,16 @@
+// Write a program to print all factors of a given number.
+
+#include <stdio.h>
+
+int main(){
+    int i,n;
+
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+for(int i=1;i<=n;i++){
+    if(n%i==0){
+        printf("%d",i );
+    }
+}
+}
